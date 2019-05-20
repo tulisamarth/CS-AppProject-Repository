ad065d65cf7f7c75dcd6f3b4a49571c141c3ff06
tulisamarth <39885124+tulisamarth@users.noreply.github.com>
2019-05-20 11:34:45 -0400

    NutritionApp.js Commit
    Set up first file for Nutrition App and imported basic components for React Native.

---
+++ finalProjectApp/NutritionApp.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { AppRegistry, View, Text, StyleSheets, TouchableHighlight, TextInput, Image, ImageBackground, Button, Video, Audio } from 'react-native';
+import { Constants } from 'expo';
+
+export default class App extends Component {
+  render{
+    return(
+
+    );
+  }
+}
+
+
+const appStyleSheet = StyleSheets.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'blue'
+  }
+});
