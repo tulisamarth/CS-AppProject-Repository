dbd7b77d3c52931fa8e585dfc39a35ba17a1a830
tulisamarth <39885124+tulisamarth@users.noreply.github.com>
2019-05-25 16:34:17 -0400

    Navigation Features and Styling Commit
    Reorganized layout of app, developed navigation features using StackNavigator, and produced logo to include for app in the home page.

--- finalProjectApp/TestScoreApp.js
+++ finalProjectApp/TestScoreApp.js
@@ -1,54 +1,142 @@
 import React, { Component } from 'react';
-import { AppRegistry, View, Text, StyleSheet, TouchableHighlight, TextInput, Image, ImageBackground, Button, Video, Audio } from 'react-native';
+import { AppRegistry, View, Text, StyleSheet, TouchableHighlight, TextInput, Image, ImageBackground, Button, Video, Audio, Alert } from 'react-native';
 import { Constants } from 'expo';
+import {createStackNavigator, createAppContainer, NavigationActions, StackActions, StackNavigator} from 'react-navigation';
 
-export default class App extends Component { //Need to complete primary window, introduce Pan Responder for test performance analysis, and
-                                            //connect website/app resources for test recommendations. (Next priority with Pan Responder and datasets.)
+class HomeScreen extends Component {
+  static navigationOptions = {
+    title: 'Home Page'
+  }
+
+  render() {
+    return (
+      <View style = {styles.container}>
+        <Text style = {styles.appTitle}>
+          Test Scores Navigator
+        </Text>
+        <Text style = {styles.title}>
+          Welcome to the Home Screen!
+        </Text>
+
+        <View style = {{flex: 1, alignItems: 'center'}}>
+          <TouchableHighlight
+            onPress = {() => Alert.alert("Test Navigator Mission: We desire to provide you an effective way to compute your test scores and compare them against national statistics.")}
+          >
+            <Image
+              source = {{uri: 'https://dwtyzx6upklss.cloudfront.net/Pictures/2000x2000fit/2/8/5/5285_marketmapeducationlogo_45714.png'}}
+              style = {styles.appLogo}
+            />
+          </TouchableHighlight>
+
+          <TouchableHighlight
+            style = {styles.introButton}
+            onPress = {() => this.props.navigation.navigate('TestPage')}
+          >
+            <Text style = {styles.buttonText}>
+              Enter Test Score Data
+            </Text>
+          </TouchableHighlight>
+        </View>
+      </View>
+    );
+  }
+}
+
+class TestScoresScreen extends Component{
+  static navigationOptions = {
+    title: 'Test Scores Page',
+  }
+
   state = {
     currentTestScore: 0.0,
+    scoreTotal: 0.0,
     averageTestScore: 0.0,
-    scoreCount: 0,
+    scoreCount: 0.0,
     className: "",
     classData: [[]],
+    classIndex: 0,
+    classWeight: 0.0,
+    scoresFinished: false
   }
 
-  calculateAverageScore = () => {
-    this.setState({averageTestScore: parseFloat(this.state.averageTestScore)/this.state.scoreCount})
+  /*
+  renderLine = (className, avg) => {
+    return(
+    <View style = {styles.subContainer}>
+      <Text style = {styles.scoreDataText}>
+        Class: {className}, Average Score: {avg}
+        {"\n"}
+      </Text>
+    </View>
+    );
   }
+
+  renderScores = () => {
+    for (var i = 0; i<this.state.classData.length; i++){
+      {this.renderLine(this.state.classData[i][0], this.state.classData[i][1])}
+    }
+  }
+  */
 
   addClassData = () => {
-    this.setState({classData: this.state.classData + [[this.state.className, this.state.averageTestScore]]})
+    this.setState({classData: this.state.classData + [[this.state.className, (1.0 * this.state.scoreTotal/this.state.scoreCount) + (this.state.classWeight * 10)]]}),
+    this.setState({classIndex: this.state.classIndex + 1})
+    console.log("/n");
+  }
+
+  finishScores = () => {
+    this.setState({scoresFinished: true})
+    //{this.renderScores}
   }
 
   resetScore = () => {
     this.setState({averageTestScore: 0}),
     this.setState({currentTestScore: 0}),
     this.setState({scoreCount: 0}),
-    this.setState({className: ""})
+    this.setState({className: ""}),
+    this.setState({scoreTotal: 0}),
+    this.setState({classWeight: 0.0})
   }
 
-  inputScore = testScore => {
-    this.setState({currentTestScore: parseInt(testScore, 10)})
+  eraseHistory = () => {
+    this.setState({classData: [[]]}),
+    this.setState({classIndex: 0})
+  }
+
+  calculateAverageScore = () => {
+     this.setState({averageTestScore: parseFloat(this.state.scoreTotal)/this.state.scoreCount}),
+     this.setState({classIndex: this.state.classIndex + 1})
   }
 
   inputClass = subjectName => {
     this.setState({className: subjectName})
   }
 
-  addScore = () => {
-    this.setState({averageTestScore: this.state.averageTestScore + this.state.currentTestScore})
-    this.setState({scoreCount: this.state.scoreCount + 1})
+  inputScore= testScore => {
+    this.setState({currentTestScore: parseInt(testScore, 10)})
   }
+
+  inputClassWeight = weight => {
+    this.setState({classWeight: parseFloat(weight, 10)})
+  }
+
+  addScore = () => {
+    this.setState({scoreTotal: this.state.scoreTotal*1.0+this.state.currentTestScore*1.0}),
+    this.setState({scoreCount: this.state.scoreCount*1.0 + 1.0}),
+    this.setState({averageTestScore: parseFloat(this.state.scoreTotal)/this.state.scoreCount})
+  }
+
+
 
   render()
   {
-    return(
+    return (
       <View style = {styles.container}>
         <Text style = {styles.title}>
-          Test Scores Average App
+          Welcome to the test scores screen!
         </Text>
 
-        <Text style = {styles.title}>
+        <Text style = {styles.questionTitle}>
           What class are you entering your test scores for?
         </Text>
 
@@ -59,7 +147,7 @@ export default class App extends Component { //Need to complete primary window, 
         >
         </TextInput>
 
-        <Text style = {styles.title}>
+        <Text style = {styles.questionTitle}>
           What test score would you like to enter?
         </Text>
 
@@ -70,17 +158,19 @@ export default class App extends Component { //Need to complete primary window, 
         >
         </TextInput>
 
-          <View style = {styles.subContainer}>
+        <Text style = {styles.questionTitle}>
+          What is the associated weight for this class?
+        </Text>
 
-            <TouchableHighlight
-              style = {styles.button}
-              onPress = {this.calculateAverageScore}
-            >
-              <Text style = {styles.buttonText}>
-                Calculate Average Score
-              </Text>
-            </TouchableHighlight>
+        <TextInput
+          value = {this.state.classWeight}
+          style = {styles.textInputField}
+          onChangeText = {this.inputClassWeight}
+        >
+        </TextInput>
 
+
+         <View style = {styles.subContainer}>
             <TouchableHighlight
               style = {styles.button}
               onPress = {this.addScore}
@@ -107,24 +197,111 @@ export default class App extends Component { //Need to complete primary window, 
                 Add Class Information
               </Text>
             </TouchableHighlight>
+
+            <TouchableHighlight
+              style = {styles.button}
+              onPress = {this.eraseHistory}
+            >
+              <Text style = {styles.buttonText}>
+                Erase Test History
+              </Text>
+            </TouchableHighlight>
+
+            <TouchableHighlight
+              style = {styles.button}
+              onPress = {this.finishScores}
+            >
+              <Text style = {styles.buttonText}>
+                Finish Class Scores
+              </Text>
+            </TouchableHighlight>
           </View>
+
+
+         <View style = {styles.subContainer}>
+            <Text style = {styles.testText}>
+              Average Test Score for {this.state.className}-> {this.state.scoreTotal/this.state.scoreCount}
+            </Text>
+          </View>
+        <View style = {styles.subContainer}>
+            <Text style = {styles.testText}>
+              Test Count: {this.state.scoreCount}, Total Score: {this.state.scoreTotal}
+            </Text>
+          </View>
+
+          <Text style= {styles.testText}>
+              Class History with Names and Average Scores:
+          </Text>
+
+          <Text style = {styles.scoreDataText}>
+            Subject {this.state.classIndex}: {this.state.className}, Weighted Average Score: {1.0 *(this.state.scoreTotal/this.state.scoreCount) + (this.state.classWeight * 10)}
+            {'\n'}
+          </Text>
+
+          <Text style = {styles.scoreDataText}>
+            {this.state.classData}
+          </Text>
 
           <View style = {styles.subContainer}>
-
-            <Text style = {styles.buttonText}>
-              {this.state.averageTestScore}
-            </Text>
-
-            <Text style= {styles.testScoreText}>
-              Test Score History:
-            </Text>
-
-            <Text style= {styles.testScoreText}>
-              {this.state.classData}
-            </Text>
+            <TouchableHighlight
+              style = {styles.analysisButton}
+              onPress = {() => this.props.navigation.navigate('AnalysisPage')}
+            >
+              <Text style = {styles.buttonText}>
+                Analyze Your Test Score Data
+              </Text>
+            </TouchableHighlight>
           </View>
-      </View>
 
+        </View>
+    );
+  }
+}
+
+class AnalysisScreen extends Component{
+  static navigationOptions = {
+    title: 'Analysis Page',
+  }
+
+  render() {
+    return (
+      <View style = {styles.container}>
+        <Text style = {styles.title}>
+          Welcome to the Analysis Screen!
+
+        </Text>
+        <Text style = {styles.analysisText}>
+          Analyzing your results....
+        </Text>
+
+        <View style = {styles.subContainer}>
+          <TouchableHighlight
+            style = {styles.analysisButton}
+            onPress = {() => this.props.navigation.navigate('RecommendationPage')}
+          >
+            <Text style = {styles.buttonText}>
+              View Your Test Recommendations
+            </Text>
+          </TouchableHighlight>
+        </View>
+      </View>
+    );
+  }
+}
+
+
+class RecommendationScreen extends Component {
+  static navigationOptions = {
+    title: 'Recommendations Page',
+  }
+
+  render() {
+    return (
+      <View style = {styles.container}>
+        <Text style = {styles.title}>
+          Welcome to the Recommendations Page!
+        </Text>
+      </View>
     );
   }
 }
@@ -133,23 +310,50 @@ export default class App extends Component { //Need to complete primary window, 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'blue',
+    backgroundColor: 'cyan',
+  },
+  appTitle: {
+    marginTop: 20,
+    color: 'blue',
+    textAlign: 'center',
+    fontSize: 30,
+    fontFamily: 'Calibri',
+    marginBottom: 10,
+    fontWeight: 'bold'
+  },
+  appLogo: {
+    marginTop: 20,
+    width: 300,
+    height: 300,
+    alignItems: 'center',
+    justifyContent: 'center',
+    imageAlign: 'center'
   },
   subContainer: {
     alignItems: 'center',
     justifyContent: 'center',
-    flexDirection: 'row',
+    flexDirection: 'row'
   },
   title: {
     marginTop: 20,
     color: 'black',
     textAlign: 'center',
+    fontSize: 30,
+    fontFamily: 'Calibri',
+    marginBottom: 10,
+    fontWeight: 'bold'
+  },
+  questionTitle: {
+    marginTop: 20,
+    color: 'red',
+    textAlign: 'center',
     fontSize: 15,
     fontFamily: 'Calibri',
-    marginBottom: 10
+    marginBottom: 10,
+    fontWeight: 'bold'
   },
   textInputField: {
-    borderColor: 'yellow',
+    borderColor: 'black',
     borderWidth: 1,
     width: 100,
     height: 20,
@@ -158,27 +362,79 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     marginLeft: 140
   },
-  button: {
+  introButton: {
     marginTop: 25,
-    width: 80,
+    width: 100,
     height: 45,
     backgroundColor: 'white',
     alignItems: 'center' ,
     justifyContent: 'center',
     flexDirection: 'row',
-    marginRight: 10
+    marginRight: 10,
+    borderWidth: 1,
+    borderColor: 'black'
+  },
+  button: {
+    marginTop: 25,
+    width: 55,
+    height: 45,
+    backgroundColor: 'white',
+    alignItems: 'center' ,
+    justifyContent: 'center',
+    flexDirection: 'row',
+    marginRight: 10,
+    borderWidth: 1,
+    borderColor: 'black'
+  },
+  analysisButton: {
+    width: 210,
+    height: 40,
+    backgroundColor: 'white',
+    alignItems: 'center' ,
+    justifyContent: 'center',
+    flexDirection: 'row',
+    marginRight: 10,
+    borderWidth: 1,
+    borderColor: 'black'
   },
   buttonText: {
     fontFamily: 'Helvetica',
     fontColor: 'black',
-    textAlign: 'center'
-  },
-  testScoreText: {
-    fontSize: 15,
-    color: 'white',
-    fontFamily: 'Arial',
-    marginTop: 40,
     textAlign: 'center',
-    marginBottom: 20
+    marginTop: 10
+  },
+  testText: {
+    fontSize: 15,
+    color: 'blue',
+    fontFamily: 'Arial',
+    marginTop: 10,
+    textAlign: 'center',
+    marginBottom: 10,
+    fontWeight: 'bold'
+  },
+  scoreDataText: {
+    fontSize: 14,
+    marginTop: 5,
+    fontFamily: 'Arial',
+    color: 'black',
+    textAlign: 'center',
+  },
+  analysisText: {
+    fontFamily: 'Helvetica',
+    fontSize: 14,
+    textAlign: 'center'
   }
+
 });
+
+
+const AppNavigator = createStackNavigator({
+  HomePage: {screen: HomeScreen},
+  TestPage: {screen: TestScoresScreen},
+  AnalysisPage: {screen: AnalysisScreen},
+  RecommendationPage: {screen: RecommendationScreen},
+});
+
+const TestScoresApp = createAppContainer(AppNavigator);
+
+export default TestScoresApp;
